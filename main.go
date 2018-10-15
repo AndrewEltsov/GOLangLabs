53af65ee159df7a69a4ecdef42418a61b385805e
AndrewEltsov <andrei.eltzov@gmail.com>
2018-10-15 21:04:26 +0300

    Now program is able to create a Signed Zip Package (szp-archive)

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"crypto/sha1"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/binary"
 	"flag"
 	"fmt"
+	"hash"
+	"io"
 	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"github.com/fullsailor/pkcs7"
+	yaml "gopkg.in/yaml.v2"
 )
 
 func main() {
@@ -19,14 +33,8 @@ func main() {
 		if err != nil {
 			fmt.Printf("Error has occured: %s", err.Error())
 		}
-		fmt.Println(files)
 
-		metas, archive, err := zipFiles(files)
-		if err != nil {
-			fmt.Printf("Error has occured: %s", err.Error())
-		}
-
-		err = createSzp("data.szp", "my.key", "my.crt", metas, archive)
+		err = createSzp(files, "data.szp", "my.key", "my.crt")
 		if err != nil {
 			fmt.Printf("Error has occured: %s", err.Error())
 		}
@@ -64,12 +72,166 @@ func getFileList(dir string) ([]string, error) {
 	return fileNames, nil
 }
 
-func zipFiles(files []string) (metas, archive []byte, err error) {
+func zipFiles(files []string) (archive []byte, err error) {
+	buf := new(bytes.Buffer)
 
-	return nil, nil, nil
+	w := zip.NewWriter(buf)
+
+	for _, file := range files {
+		fileBytes, err := ioutil.ReadFile(file)
+		if err != nil {
+
+			return nil, err
+		}
+
+		f, err := w.Create(file[1:])
+		if err != nil {
+			return nil, err
+		}
+		_, err = f.Write(fileBytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = w.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	//ioutil.WriteFile("bufer", buf.Bytes(), 0666)
+	return buf.Bytes(), nil
 }
 
-func createSzp(newFile, keyPath, certPath string, meta, archive []byte) error {
+func createSzp(files []string, newFile, keyPath, certPath string) error {
+	metas, err := createMeta(files)
+	if err != nil {
+		return err
+	}
+
+	archive, err := zipFiles(files)
+	if err != nil {
+		return err
+	}
+
+	metaLength := make([]byte, 4)
+	binary.LittleEndian.PutUint32(metaLength, uint32(len(metas)))
+
+	data := append(metaLength, metas...)
+	data = append(data, archive...)
+
+	szp, err := signFile(data, keyPath, certPath)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(newFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(szp)
+	if err != nil {
+		return err
+	}
 
 	return nil
+}
+
+func createMeta(files []string) (metaBytes []byte, err error) {
+	metas := make(map[string]meta)
+
+	var metaInfo meta
+	var h hash.Hash
+
+	for i := range files {
+		openedFile, err := os.Open(files[i])
+		if err != nil {
+			return nil, err
+		}
+		info, err := openedFile.Stat()
+		if err != nil {
+
+			return nil, err
+		}
+
+		metaInfo.Name = info.Name()
+		metaInfo.Size = info.Size()
+		metaInfo.ModTime = info.ModTime()
+
+		h = sha1.New()
+		if _, err := io.Copy(h, openedFile); err != nil {
+			return nil, err
+		}
+
+		metaInfo.Hash = string(h.Sum(nil))
+
+		metas[files[i][1:]] = metaInfo
+
+		err = openedFile.Close()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	bytesToZip, err := yaml.Marshal(metas)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := new(bytes.Buffer)
+	w := zip.NewWriter(buf)
+
+	f, err := w.Create("metas")
+	if err != nil {
+		return nil, err
+	}
+	_, err = f.Write(bytesToZip)
+	if err != nil {
+		return nil, err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	//ioutil.WriteFile("bufer", buf.Bytes(), 0666)
+	return buf.Bytes(), nil
+}
+
+func signFile(fileBytes []byte, keyPath, certPath string) ([]byte, error) {
+	signedData, err := pkcs7.NewSignedData(fileBytes)
+	if err != nil {
+		fmt.Printf("Cannot initialize signed data: %s", err)
+	}
+
+	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parsedCert, err := x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		return nil, err
+	}
+
+	if err := signedData.AddSigner(parsedCert, cert.PrivateKey, pkcs7.SignerInfoConfig{}); err != nil {
+		return nil, err
+	}
+
+	signedFileBytes, err := signedData.Finish()
+	if err != nil {
+		return nil, err
+	}
+
+	return signedFileBytes, nil
+}
+
+type meta struct {
+	Name    string
+	Size    int64
+	ModTime time.Time
+	Hash    string
 }
