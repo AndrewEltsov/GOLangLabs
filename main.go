1d16ee4a3580aa73344d7b9ad9815550c43ea87d
AndrewEltsov <andrei.eltzov@gmail.com>
2018-10-15 19:49:50 +0300

    Function getFileList was implemented.

--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 
 	var modePtr string
-	flag.StringVar(&modePtr, "mode", "i", "choosing program mode")
+	flag.StringVar(&modePtr, "mode", "z", "choosing program mode")
 
 	flag.Parse()
 
 	switch modePtr {
 	case "z":
-		files := getFileList("./mydir")
+		files, err := getFileList("./mydir")
+		if err != nil {
+			fmt.Printf("Error has occured: %s", err.Error())
+		}
+		fmt.Println(files)
 
 		metas, archive, err := zipFiles(files)
 		if err != nil {
@@ -36,9 +41,27 @@ func main() {
 }
 
 //Receives name of directory to zip and returns list of files to zip
-func getFileList(dir string) []string {
+func getFileList(dir string) ([]string, error) {
+	var fileNames []string
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil
+	for _, file := range files {
+		if file.IsDir() {
+			subdirfiles, err := getFileList(dir + "/" + file.Name())
+			if err != nil {
+				return nil, err
+			}
+
+			fileNames = append(fileNames, subdirfiles...)
+		} else {
+			fileNames = append(fileNames, dir+"/"+file.Name())
+		}
+	}
+
+	return fileNames, nil
 }
 
 func zipFiles(files []string) (metas, archive []byte, err error) {
